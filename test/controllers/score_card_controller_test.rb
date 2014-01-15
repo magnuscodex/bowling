b3e42f28a9045cbf7a099518a62e4cef9a969b2a
Mike Roberts <magnuscodex@gmail.com>
2014-01-14 19:20:49 -0800

    Added some more test coverage. Still incomplete.

--- test/controllers/score_card_controller_test.rb
+++ test/controllers/score_card_controller_test.rb
@@ -22,11 +22,18 @@ class ScoreCardControllerTest < ActionController::TestCase
     assert_equal(assigns["error_val"][0], "has-error")
   end
 
-  test "Strike Score" do
+  test "Spare Score" do
     post(:new, {"Frame1_bowl1" => 3, "Frame1_bowl2" => 7,
                 "Frame2_bowl1" => 7, "Frame2_bowl2" => 1})
     assert_equal(assigns["scores"][0], 17)
     assert_equal(assigns["scores"][1], 25)
+  end
+
+  test "Strike Score" do
+    post(:new, {"Frame1_bowl1" => 10, "Frame1_bowl2" => 0,
+                "Frame2_bowl1" => 7, "Frame2_bowl2" => 1})
+    assert_equal(assigns["scores"][0], 18)
+    assert_equal(assigns["scores"][1], 26)
   end
 
   test "Last Frame strike" do
@@ -34,4 +41,15 @@ class ScoreCardControllerTest < ActionController::TestCase
                 "Frame10_bowl3" => 10})
     assert_equal(assigns["scores"][9], 22)
   end
+
+  test "Error in entry score" do
+    post(:new, {"Frame1_bowl1" => 1, "Frame1_bowl2" => 3,
+                "Frame2_bowl1" => 7, "Frame2_bowl2" => 4})
+    assert_equal(assigns["scores"][0], 4)
+    assert_equal(assigns["scores"][1], nil)
+  end
+
+  #TODO: Obviously more test coverage is needed. But this is an acceptable
+  #      first pass.
+
 end
